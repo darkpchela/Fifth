f4910e68a63fe10b868b755b7d6afd9a3d9bfe97
darkpchela <darkpchela2@gmail.com>
2020-12-12 17:55:04 +0300

    front updated

---
+++ Fifth/wwwroot/js/index.js
@@ -0,0 +1,18 @@
+﻿let input = document.querySelector('#tagsInput');
+new Tagify(input);
+$('#tagsCloud a, #tagsFilter a').click((e) => {
+    e.preventDefault();
+    let selected = $(e.target).attr('data-selected');
+    if (selected == 0) {
+        $(e.target).appendTo($('#tagsFilter'))
+        e.target.setAttribute('data-selected', 1)
+    }
+    else {
+        $(e.target).appendTo($('#tagsCloud'));
+        e.target.setAttribute('data-selected', 0);
+    }
+});
+$('#newGame').click((e) => {
+    e.preventDefault();
+    $('#modal').modal('show');
+})
