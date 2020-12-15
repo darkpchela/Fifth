77e12e287ce1d28594ab312e4c41293ba38844a9
darkpchela <darkpchela2@gmail.com>
2020-12-16 02:05:18 +0300

    view js updated

--- Fifth/wwwroot/js/index.js
+++ Fifth/wwwroot/js/index.js
@@ -1,13 +1,1 @@
-﻿$('#tagsCloud a, #tagsFilter a').click((e) => {
-    e.preventDefault();
-    let selected = $(e.target).attr('data-selected');
-    if (selected == 0) {
-        $(e.target).appendTo($('#tagsFilter'))
-        e.target.setAttribute('data-selected', 1)
-    }
-    else {
-        $(e.target).appendTo($('#tagsCloud'));
-        e.target.setAttribute('data-selected', 0);
-    }
-});
-
+﻿
